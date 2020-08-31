21b5b61e5ac1a4ded2edadfde45c8e73e70c77df
bhawnapandey <bhawnapandey127@gmail.com>
2020-08-31 10:50:32 +0530

    changes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const cheerio = require('cheerio');
 
 const url = "https://www.imdb.com/title/tt7286456/?ref_=hm_fanfav_tt_2_pd_fp1";
 
-unirest.get(url).then((data)=>{
-   const $ = cheerio.load(data);
-   let title = $('div[class="title_wrapper"]').html();
-   console.log("title", title);
+unirest.get(url).then((response)=>{
+
+    const $ = cheerio.load(response.body);
+    let title = $('div[class="title_wrapper"] > h1').text();
+    let rating = $('span[itemprop="ratingValue"]').text();
+    console.log(title, rating);
+
 }).catch((error)=> {
    console.log("error", error);
 });
