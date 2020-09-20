e85c4a5bdf6f024588cdb2c7a9afbc270a8d99d1
bhawnapandey <bhawnapandey127@gmail.com>
2020-09-21 00:04:24 +0530

    get the title and url of the page using puppeteer

--- index.js
+++ index.js
@@ -13,12 +13,22 @@ const puppeteer = require('puppeteer');
   // await browser.close();
 
   /* Example Generating pdf */
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto('https://www.udemy.com/');
+    // await page.pdf({
+    //   path: './page.pdf',
+    //   format: 'A4'
+    // });
+    // await browser.close();
+
+    /*Get the url or title of the page */
+    const browser = await puppeteer.launch({ headless: false});
     const page = await browser.newPage();
     await page.goto('https://www.udemy.com/');
-    await page.pdf({
-      path: './page.pdf',
-      format: 'A4'
-    });
+    let title = await page.title();
+    console.log("the title of the page", title);
+    let url = await page.url();
+    console.log("the url of the page", url);
     await browser.close();
 })();
