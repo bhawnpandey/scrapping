810b8b8339f77c0cb3b80db298e20f671b61f4f2
bhawnapandey <bhawnapandey127@gmail.com>
2020-10-04 00:15:43 +0530

    login with twitter

--- index.js
+++ index.js
@@ -1,69 +1,15 @@
 const puppeteer = require('puppeteer');
-const galaxy = puppeteer.devices['Galaxy Note 3'];
-(async () => {
-  /*Simple example of puppeteer */
-
-  // const browser = await puppeteer.launch({
-  //   headless: false
-  // });
-  // const page = await browser.newPage();
-  // await page.goto('https://www.google.com/');
-  // await page.type('.gLFyf', 'Data structure', {delay: 100});
-  // await page.screenshot({path: 'example.png'});
-  //
-  // await browser.close();
-
-  /* Example Generating pdf */
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
-    // await page.goto('https://www.udemy.com/');
-    // await page.pdf({
-    //   path: './page.pdf',
-    //   format: 'A4'
-    // });
-    // await browser.close();
-
-    /*Get the url or title of the page */
-    // const browser = await puppeteer.launch({ headless: false});
-    // const page = await browser.newPage();
-    // await page.goto('https://www.udemy.com/');
-    // let title = await page.title();
-    // console.log("the title of the page", title);
-    // let url = await page.url();
-    // console.log("the url of the page", url);
-    // await browser.close();
-
-    /*Emulate a phone view*/
-   // const browser = await puppeteer.launch({headless: false});
-   // const page = await browser.newPage();
-   // await page.emulate(galaxy);
-   // await page.goto('https://www.udemy.com/');
-   // await browser.close();
-
-   /*setRequestInterception of page*/
-   // const browser = await puppeteer.launch({headless: false});
-   // const page = await browser.newPage();
-   // await page.setRequestInterception(true);
-   // page.on('request', interceptedRequest => {
-   //   if(["image","stylesheet","font"].includes(interceptedRequest.resourceType())){
-   //     interceptedRequest.abort();
-   //   }else{
-   //     interceptedRequest.continue();
-   //   }
-   // });
-   // await page.goto('https://www.amazon.in/');
-   // await browser.close();
-
-   /*Ignoring HTTPS/SSL Errors and changing the viewport*/
-   const browser = await puppeteer.launch({
-     headless: false,
-     ignoreHTTPSErrors: true,
-     defaultViewport:{
-       width: 1000,
-       height: 1000
-     }
-   });
+(async ()=>{
+   const base_url = "https://twitter.com/";
+   const login_url = "https://twitter.com/login";
+   const username = "xxxxxxxxx";
+   const password = "xxxxxxxxxx";
+   const browser = await puppeteer.launch({headless: false});
    const page = await browser.newPage();
-   await page.goto('https://www.google.com/');
-   await browser.close();
+   await page.goto(login_url);
+   await page.waitForSelector('input[name="session[username_or_email]"]');
+   await page.type('input[name="session[username_or_email]"]', username);
+   await page.type('input[name="session[password]"]', password);
+   await page.click('#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(8) > div');
+  await browser.close();
 })();
