0e32de8543ad955c130b3475ebbe762f2a9db8d5
bhawnapandey <bhawnapandey127@gmail.com>
2020-10-10 00:24:15 +0530

    tweet post

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let twitter = require('./twitter.js');
 (async ()=>{
 
-   const username = "xxxxxxxxxxxxxx";
-   const password = "xxxxxxxxxxxxxx";
+   const username = "xxxxxxxxxxxxx";
+   const password = "xxxxxxxxxxxxx";
 
    await twitter.initialize();
    await twitter.login(username, password);
+   await twitter.postTweet("Hello everyone, this message for testing");
    await twitter.end();
 
 
--- twitter.js
+++ twitter.js
@@ -20,6 +20,18 @@ const twitter = {
        await page.click('#react-root > div > div > div.css-1dbjc4n.r-13qz1uu.r-417010 > main > div > div > div.css-1dbjc4n.r-13qz1uu > form > div > div:nth-child(8) > div');
      },
 
+     postTweet: async(message)=> {
+        let url = await page.url();
+        if(url != base_url){
+          await page.goto(base_url);
+        }
+        await page.waitForSelector('div[data-testid="tweetTextarea_0"]');
+        await page.click('div[data-testid="tweetTextarea_0"]');
+        await page.waitForTimeout(500);
+        await page.keyboard.type(message, {delay: 50});
+        await page.click("#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-1tlfku8.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-184en5c > div > div.css-1dbjc4n.r-14lw9ot.r-atwnbb > div:nth-child(1) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-glunga.r-1bylmt5.r-13tjlyg.r-7qyjyx.r-1ftll1t > div:nth-child(4) > div > div > div:nth-child(2) > div.css-18t94o4.css-1dbjc4n.r-urgr8i.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-1w2pmg.r-1n0xq6e.r-1vuscfd.r-1dhvaqw.r-1ny4l3l.r-1fneopy.r-o7ynqc.r-6416eg.r-lrvibr > div");
+     },
+
      end: async() =>{
         await browser.close();
      }
